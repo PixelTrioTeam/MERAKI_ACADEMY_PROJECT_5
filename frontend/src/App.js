9826e16b246f10a6fee2ec78ddcb18b9a24a4c4f
AbdullahKhalaaf <Abdullah.Khalaaf@gmail.com>
2025-01-31 15:47:31 +0300

    starterCode

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+
+
+//===============================================================
+
+const App = () => {
+  return (
+    <div className="App">
+      
+    
+    </div>
+  );
+};
+
+export default App;
