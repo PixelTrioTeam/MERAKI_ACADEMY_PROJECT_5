785072b6ddc18e1b051847beab528583b94d0781
MustafaMsallam <mustafamsallam04@gmail.com>
2025-02-01 19:58:46 +0300

    login and register user

---
+++ backend/controllers/user.js
@@ -0,0 +1,89 @@
+const pool = require("../models/db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const saltRounds = parseInt(process.env.SALT);
+
+const register = async (req, res) => {
+  const { firstName, lastName, email, password, country, created_at } =
+    req.body;
+  const role_id = "2"; // edit the value of role_id depend on role id in role table .
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+  const query = `INSERT INTO users (firstName, lastName,email ,password ,country, role_id, created_at) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
+  const data = [
+    firstName,
+    lastName,
+    email.toLowerCase(),
+    encryptedPassword,
+    country,
+    role_id,
+    created_at,
+  ];
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "Account created successfully",
+      });
+    })
+    .catch((err) => {
+      res.status(409).json({
+        success: false,
+        message: "The email already exists",
+      });
+    });
+};
+
+const login = (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  const query = `SELECT * FROM users WHERE email = $1`;
+  const data = [email.toLowerCase()];
+  pool
+    .query(query, data)
+    .then((result) => {
+      if (result.rows.length) {
+        bcrypt.compare(password, result.rows[0].password, (err, response) => {
+          if (err) res.json(err);
+          if (response) {
+            const payload = {
+              userId: result.rows[0].id,
+              country: result.rows[0].country,
+              role: result.rows[0].role_id,
+            };
+            const options = { expiresIn: "1d" };
+            const secret = process.env.SECRET;
+            const token = jwt.sign(payload, secret, options);
+            if (token) {
+              return res.status(200).json({
+                token,
+                success: true,
+                message: `Valid login credentials`,
+                userId: result.rows[0].id,
+              });
+            } else {
+              throw Error;
+            }
+          } else {
+            res.status(403).json({
+              success: false,
+              message: `The email doesn’t exist or the password you’ve entered is incorrect`,
+            });
+          }
+        });
+      } else throw Error;
+    })
+    .catch((err) => {
+      res.status(403).json({
+        success: false,
+        message:
+          "The email doesn’t exist or the password you’ve entered is incorrect",
+        err,
+      });
+    });
+};
+
+module.exports = {
+  register,
+  login,
+};
--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 require("./models/db");
-const SeriesRouter = require('./routes/series')
-const movieRouter = require('./routes/movie')
+const SeriesRouter = require("./routes/series");
+const movieRouter = require("./routes/movie");
+const userRouter = require("./routes/user");
 //routers
 
 const app = express();
@@ -11,8 +12,9 @@ const app = express();
 //built-in middleware
 app.use(express.json());
 app.use(cors());
-app.use('/series' , SeriesRouter)
-app.use('/movie' , movieRouter)
+app.use("/series", SeriesRouter);
+app.use("/movie", movieRouter);
+app.use("/user", userRouter);
 // router middleware
 
 const PORT = process.env.PORT || 5000;
