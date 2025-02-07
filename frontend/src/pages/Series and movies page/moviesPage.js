28905c2b65a14a710f43f4be4c705a3625c0330f
OsamaTuffaha <osamamohdtuffaha@gmail.com>
2025-02-07 20:04:29 +0300

    add genre for movies and series

--- frontend/src/pages/Series and movies page/moviesPage.js
+++ frontend/src/pages/Series and movies page/moviesPage.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { setMovies } from "../../service/redux/reducers/movies/movieSlice";
-import "./movies.css";
+import { setMovies } from '../../Service/redux/reducers/movies/movieSlice';
 import { useNavigate } from "react-router-dom";
+import "./movies.css";
 
 const MoviesPage = () => {
   const dispatch = useDispatch();
@@ -19,35 +19,37 @@ const MoviesPage = () => {
       .catch((err) => console.error(err));
   }, [dispatch]);
 
+  const genres = ["Action", "Comedy", "Drama", "Horror", "Sci-Fi"];
+
   return (
     <div className="movies-container">
-      <h1>Movies Page</h1>
-      <div className="movies-grid">
-        {movies.length > 0 ? (
-          movies.map((movie) => (
-            <div className="flip-card" key={movie.id}>
-              <div className="flip-card-inner">
-                <div className="flip-card-front">
-                  <img
-                    src={movie.poster}
-                    alt={movie.title}
-                    className="movie-image"
-                  />
+      
+      {genres.map((genre) => (
+        <section key={genre} className="genre-section">
+          <h2 className="genre-title">{genre}</h2>
+          <div className="movies-grid">
+            {movies.length > 0 ? (
+              movies.filter(movie => movie.genre_name === genre).map((movie) => (
+                <div className="flip-card" key={movie.id}>
+                  <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                      <img src={movie.poster} alt={movie.title} className="movie-image"/>
+                    </div>
+                    <div className="flip-card-back">
+                      <h3 className="movie-title">{movie.title}</h3>
+                      <p className="movie-description">{movie.description}</p>
+                      <p className="movie-rating">⭐ {movie.rate}/10</p>
+                      <button className="more-button" onClick={() => navigate(`/movies/${movie.id}`)}>More Info</button>
+                    </div>
+                  </div>
                 </div>
-
-                <div className="flip-card-back">
-                  <h2 className="movie-title">{movie.title}</h2>
-                  <p className="movie-description">{movie.description}</p>
-                  <p className="movie-rating">⭐ {movie.rate}/10</p>
-                  <button className="more-button">More</button>
-                </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
+              ))
+            ) : (
+              <p className="loading-text">Fetching movies...</p>
+            )}
+          </div>
+        </section>
+      ))}
     </div>
   );
 };
