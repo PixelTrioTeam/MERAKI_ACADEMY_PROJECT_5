50cf26fcc5ff0aab7d34b24af12f8b9f3220f948
MustafaMsallam <mustafamsallam04@gmail.com>
2025-02-07 21:22:56 +0300

    homePage

--- frontend/src/Routers/index.js
+++ frontend/src/Routers/index.js
@@ -2,10 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import Main from "../layouts/Main/index";
 import MoviesPage from "../pages/Series and movies page/moviesPage";
-
+import WelcomePage from "../pages/welcomePage/WelcomePage";
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <WelcomePage />,
+  },
+  {
+    path: "/main",
     element: <Main />,
     children: [
       {
