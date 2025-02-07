b6f61a44cc06139af7868941993c6420548441f7
OsamaTuffaha <osamamohdtuffaha@gmail.com>
2025-02-07 15:57:43 +0300

    show the movie when i click on movies

--- frontend/src/Routers/index.js
+++ frontend/src/Routers/index.js
@@ -1,13 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import HomePage from "../pages/HomePage/HomePage";
-import Main from '../layouts/Main/index'
-import Navbar from "../layouts/Main/index";
+import Main from "../layouts/Main/index"; 
+import MoviesPage from "../pages/Series and movies page/moviesPage";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-   
+  },
+  {
+    path: "/movies",  
+    element: <MoviesPage />,
   },
 ]);
 
--- frontend/src/service/redux/store.js
+++ frontend/src/service/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/auth/authSlice";
+import moviesReducer from './reducers/movies/movieSlice'
 
 const store = configureStore({
   reducer: {
     authReducer: authReducer,
+    movies : moviesReducer
   },
 });
 
