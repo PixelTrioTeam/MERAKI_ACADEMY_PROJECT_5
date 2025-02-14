5c6613d3fbc1d1594b0ce0db5d9b6d33d28a3751
OsamaTuffaha <osamamohdtuffaha@gmail.com>
2025-02-14 20:43:06 +0300

    admin edit

--- frontend/src/pages/dashboard.Admin/BasicTable.js
+++ frontend/src/pages/dashboard.Admin/BasicTable.js
@@ -1,83 +1,189 @@
-    import React, { useEffect } from "react";
-    import Table from "@mui/material/Table";
-    import TableBody from "@mui/material/TableBody";
-    import TableCell from "@mui/material/TableCell";
-    import TableContainer from "@mui/material/TableContainer";
-    import TableHead from "@mui/material/TableHead";
-    import TableRow from "@mui/material/TableRow";
-    import Paper from "@mui/material/Paper";
-    import Stack from "@mui/material/Stack";
-    import DeleteIcon from "@mui/icons-material/Delete";
-    import Button from "@mui/material/Button";
-    import { useDispatch, useSelector } from "react-redux";
-    import {
-    setUsers,
-    deleteUser,
-    } from "../../service/redux/reducers/users/usersSlice";
-    import axios from "axios";
-    import "./table.css";
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
 
-    export default function BasicTable() {
-    const users = useSelector((state) => state.users.users);
-    const dispatch = useDispatch();
+export default function MoviesSeriesTable() {
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [view, setView] = useState("");
 
-    useEffect(() => {
-        axios
-        .get("http://localhost:5000/user/")
-        .then((res) => {
-            console.log(res.data.result);
-            dispatch(setUsers(res.data.result));
-        })
-        .catch((err) => console.error(err));
-    }, [dispatch]);
+  const fetchUsers = () => {
+    axios.get("http://localhost:5000/user/")
+      .then((res) => {
+        setUsers(res.data.result);
+        setMovies([]);
+        setSeries([]);
+        setView("users");
+      })
+      .catch((err) => console.error(err));
+  };
 
-    const handleDeleteUser = (userId) => {
-        axios
-        .delete(`http://localhost:5000/user/${userId}`)
-        .then((res) => {
-            dispatch(deleteUser(userId));
-            console.log(res.data.message);
-        })
-        .catch((err) => console.error(err));
-    };
+  const fetchMovies = () => {
+    axios.get("http://localhost:5000/movie")
+      .then((res) => {
+        setMovies(res.data.result);
+        setUsers([]);
+        setSeries([]);
+        setView("movies");
+      })
+      .catch((err) => console.error(err));
+  };
 
-    return (
-        <TableContainer className="tableContainer" component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="User Table">
-            <TableHead>
-            <TableRow >
-                <TableCell><h4>First Name</h4></TableCell>
-                <TableCell align="right"><h4>Last Name</h4></TableCell>
-                <TableCell align="right"><h4>Email</h4></TableCell>
-                <TableCell align="right"><h4>Country</h4></TableCell>
-                <TableCell align="right"><h4>Action</h4></TableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-            {users.map((user) => (
-                <TableRow
-                key={user.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                <TableCell component="th" scope="row">
-                    <h6>{user.firstname}</h6>
-                </TableCell>
-                <TableCell align="right"><h6>{user.lastname}</h6></TableCell>
-                <TableCell align="right"><h6>{user.email}</h6></TableCell>
-                <TableCell align="right"><h6>{user.country}</h6></TableCell>
-                <TableCell align="right">
-                    <Button
-                    onClick={() => handleDeleteUser(user.id)}
-                    variant="outlined"
-                    startIcon={<DeleteIcon />}
-                    >
-                    Delete
-                    </Button>
-                </TableCell>
+  const fetchSeries = () => {
+    axios.get("http://localhost:5000/series")
+      .then((res) => {
+        setSeries(res.data.result);
+        setUsers([]);
+        setMovies([]);
+        setView("series");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const deleteUser = (id) => {
+    axios.delete(`http://localhost:5000/user/${id}`)
+      .then(() => setUsers(users.filter(user => user.id !== id)))
+      .catch((err) => console.error(err));
+  };
+
+  const deleteMovie = (id) => {
+    axios.put(`http://localhost:5000/movie/${id}`)
+      .then(() => setMovies(movies.filter(movie => movie.id !== id)))
+      .catch((err) => console.error(err));
+  };
+
+  const deleteSeries = (id) => {
+    axios.put(`http://localhost:5000/series/delete/${id}`)
+      .then(() => setSeries(series.filter(serie => serie.id !== id)))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div>
+      <div style={{ marginBottom: "20px", textAlign: "center" }}>
+        <Button variant="contained" color="primary" onClick={fetchUsers}>Get Users</Button>
+        <Button variant="contained" color="primary" onClick={fetchMovies} style={{ marginLeft: "10px" }}>Get Movies</Button>
+        <Button variant="contained" color="primary" onClick={fetchSeries} style={{ marginLeft: "10px" }}>Get Series</Button>
+      </div>
+      
+      {view === "users" && (
+        <>
+          <h3 style={{ textAlign: "center" }}>Users</h3>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="Users Table">
+              <TableHead>
+                <TableRow>
+                  <TableCell><h4>Name</h4></TableCell>
+                  <TableCell align="right"><h4>Email</h4></TableCell>
+                  <TableCell align="right"><h4>Actions</h4></TableCell>
                 </TableRow>
-            ))}
-            </TableBody>
-        </Table>
-        </TableContainer>
-    );
-    }
+              </TableHead>
+              <TableBody>
+                {users.map((user) => (
+                  <TableRow key={user.id}>
+                    <TableCell>{user.firstname}</TableCell>
+                    <TableCell align="right">{user.email}</TableCell>
+                    <TableCell align="right">
+                      <Button variant="outlined" color="error" onClick={() => deleteUser(user.id)}>
+                        <DeleteIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+      
+      {view === "movies" && (
+        <>
+          <h3 style={{ textAlign: "center" }}>Movies</h3>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="Movies Table">
+              <TableHead>
+                <TableRow>
+                  <TableCell><h4>Title</h4></TableCell>
+                  <TableCell align="right"><h4>poster</h4></TableCell>
+                  <TableCell align="right"><h4>Actions</h4></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {movies.map((movie) => (
+                  <TableRow key={movie.id}>
+                    <TableCell>{movie.title}</TableCell>
+                    <TableCell align="right"> <img
+          src={movie.poster}
+          alt={movie.title}
+          style={{ 
+            width: "50px", 
+            height: "75px", 
+            borderRadius: "8px", 
+            transition: "transform 0.3s ease-in-out", 
+            cursor: "pointer" 
+          }}
+        /></TableCell>
+                    <TableCell align="right">
+                      <Button variant="outlined" color="error" onClick={() => deleteMovie(movie.id)}>
+                        <DeleteIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+      
+      {view === "series" && (
+        <>
+          <h3 style={{ textAlign: "center" }}>Series</h3>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="Series Table">
+              <TableHead>
+                <TableRow>
+                  <TableCell><h4>Title</h4></TableCell>
+                  <TableCell align="right"><h4>poster</h4></TableCell>
+                  <TableCell align="right"><h4>Actions</h4></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {series.map((serie) => (
+                  <TableRow key={serie.id}>
+                    <TableCell>{serie.title}</TableCell>
+                    <TableCell align="right"> <img
+          src={serie.poster}
+          alt={serie.title}
+          style={{ 
+            width: "50px", 
+            height: "75px", 
+            borderRadius: "8px", 
+            transition: "transform 0.3s ease-in-out", 
+            cursor: "pointer" 
+          }}
+        /></TableCell>
+                    <TableCell align="right">
+                      <Button variant="outlined" color="error" onClick={() => deleteSeries(serie.id)}>
+                        <DeleteIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+    </div>
+  );
+}
